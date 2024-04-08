01492a5c286620dabfabb473da1673897cfabf8d
Anna <hannah.ivanovich@gmail.com>
2024-04-08 13:33:07 +0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from "./components/main/main";
+import Navbar from "./components/navbar/navbar";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
 
 function App() {
 
   return (
     <BrowserRouter>
+			<div className="App">
+        <Navbar />
+      </div>
 			<div className="container">
 				<Routes>
 					<Route path='/' element={<Main />} />
+					<Route path="/episodes" element={<Episodes />} />
+  				<Route path="/location" element={<Location />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
